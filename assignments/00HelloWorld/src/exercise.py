96a97eb23cf185ae033b6d0f7fc1b9683ef84757
Jorge Mario Cruz-Duarte <jorge.cruz@itesm.mx>
2021-08-05 17:29:25 -0500

    Updated codes
    only pass

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -2,5 +2,5 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,12 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    harina = float(input("Dame la harina en gramos: "))
-
-    levadura = harina*50/1000
-    
-    print("Necesitas estos gramos de levadura:",levadura)
-
-
+    pass
 
 if __name__ == '__main__':
     main()
